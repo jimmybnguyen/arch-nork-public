d5a51266dc68e03368591a1aff5c5296d118bd12
jimmybnguyen <me@jimmybnguyen.com>
2016-02-28 21:02:16 -0800

    setup

--- nork-pipe-filter/index.js
+++ nork-pipe-filter/index.js
@@ -2,3 +2,9 @@
 
 var world = require('../common/world');
 var readLine = require('readline');
+var stream = require('stream');
+
+var currentRoom = world.rooms[0];
+var inventory = [];
+
+console.log(currentRoom.description);
